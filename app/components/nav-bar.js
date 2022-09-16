4988410d614eed745a10182fe5277eebd3509687
Leandro Calvo <lcalvo.intern@copyright.com>
2022-09-16 13:00:48 +0200

    fixing

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -4,30 +4,31 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class navbarComponent extends Component {
+  @service router;
+  @service login;
+  @tracked currentUser;
 
-@service router;
-@service login;
-@tracked currentUser;
-
-constructor() {
+  constructor() {
     super(...arguments);
     this.anySessionStored();
+
   }
 
   anySessionStored() {
     this.currentUser = this.login.retrieveSessionStorage();
     console.log(this.currentUser);
     if (this.currentUser.length == 0) {
-      console.log('❌ [Copyright Clearance Center]: Necesitas estar autentificado para entrar en el sistema');
+      console.log(
+        '❌ [Copyright Clearance Center]: Necesitas estar autentificado para entrar en el sistema'
+      );
       this.router.transitionTo('login');
-
     } else {
-      console.log("✔️ [Copyright Clearance Center]: Autentificación correcta");
+      console.log('✔️ [Copyright Clearance Center]: Autentificación correcta');
     }
   }
 
   @action logOut() {
-    console.log("Está usando logOut");
+    console.log('Está usando logOut');
     this.login.leaveSession();
-  }  
+  }
 }
--- app/services/login.js
+++ app/services/login.js
@@ -1,52 +1,56 @@
 import Service from '@ember/service';
 
 export default class LoginService extends Service {
-userArray = [];
-constructor(){
+  userArray = [];
+  constructor() {
     super();
-}
+  }
 
-    saveUser(email){
-        if (email.includes('@copyright.com')) {
-            this.userArray.push({email, password: 123456 });
-            localStorage.setItem('currentUser', JSON.stringify(this.userArray));
-            let verificator = document.getElementById("verificator");
-            verificator.innerHTML = '<span class="rup-lozenge rup-lozenge--sm rup-lozenge--success" aria-describedby="lozenge-desc-8">Success!</span>'+
-            '<p id="lozenge-desc-8" class="rup-visually-hidden">A bit of description about what the lozenge represents.</p>'; 
-            let spinner = document.getElementById("loading");
-            spinner.innerHTML = '<span class="rup-loader rup-loader--sm" role="status">'+
-                                    '<span class="rup-visually-hidden">Loading...</span></span>';
-            
-             return true;
-        }else{      
-            console.log("[CCC] ❌​ Please enter a valid mail address");
-            let verificator = document.getElementById("verificator");
-            verificator.innerHTML = '<span class="rup-lozenge rup-lozenge--sm"  aria-describedby="lozenge-desc-6">Verifying ...</span>'+
-                                    '<p id="lozenge-desc-6" class="rup-visually-hidden">Verifying</p>';
-            let spinner = document.getElementById("loading");
-            spinner.innerHTML = '<span class="rup-loader rup-loader--sm" role="status">'+
-                                    '<span class="rup-visually-hidden">Loading...</span></span>';            
-            return false;
-        }
-    }
+  saveUser(email) {
+    if (email.includes('@copyright.com')) {
+      this.userArray.push({ email, password: 123456 });
+      localStorage.setItem('currentUser', JSON.stringify(this.userArray));
+      let verificator = document.getElementById('verificator');
+      verificator.innerHTML =
+        '<span class="rup-lozenge rup-lozenge--sm rup-lozenge--success" aria-describedby="lozenge-desc-8">Success!</span>' +
+        '<p id="lozenge-desc-8" class="rup-visually-hidden">A bit of description about what the lozenge represents.</p>';
+      let spinner = document.getElementById('loading');
+      spinner.innerHTML =
+        '<span class="rup-loader rup-loader--sm" role="status">' +
+        '<span class="rup-visually-hidden">Loading...</span></span>';
 
-    retrieveSessionStorage() {
-        console.log('...Comprobando datos');
-        let varLocal = localStorage.getItem('currentUser');
-        if (varLocal == null) {
-          this.userArray = [];
-          console.log('No hay usuarios logeados');
-          return this.userArray;
-        } else {
-          this.userArray = [];
-          const arr = JSON.parse(localStorage.getItem('currentUser'));
-          this.userArray = [...this.userArray, ...arr];
-          return this.userArray[0].email;
-        }
-      }
-      
-    leaveSession(){
-        localStorage.clear();
-        location.reload();
+      return true;
+    } else {
+      console.log('[CCC] ❌​ Please enter a valid mail address');
+      let verificator = document.getElementById('verificator');
+      verificator.innerHTML =
+        '<span class="rup-lozenge rup-lozenge--sm"  aria-describedby="lozenge-desc-6">Verifying ...</span>' +
+        '<p id="lozenge-desc-6" class="rup-visually-hidden">Verifying</p>';
+      let spinner = document.getElementById('loading');
+      spinner.innerHTML =
+        '<span class="rup-loader rup-loader--sm" role="status">' +
+        '<span class="rup-visually-hidden">Loading...</span></span>';
+      return false;
     }
+  }
+
+  retrieveSessionStorage() {
+    console.log('...Comprobando datos');
+    let varLocal = localStorage.getItem('currentUser');
+    if (varLocal == null) {
+      this.userArray = [];
+      console.log('No hay usuarios logeados');
+      return this.userArray;
+    } else {
+      this.userArray = [];
+      const arr = JSON.parse(localStorage.getItem('currentUser'));
+      this.userArray = [...this.userArray, ...arr];
+      return this.userArray[0].email;
+    }
+  }
+
+  leaveSession() {
+    localStorage.clear();
+    location.reload();
+  }
 }
